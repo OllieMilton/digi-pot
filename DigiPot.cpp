fa3a41f3bbcfdfebbf0aa85502eb41225ce8987b
ollie8 <ollie8@localhost>
2014-01-25 21:05:15 +0000

    now fucking well works

--- DigiPot.cpp
+++ DigiPot.cpp
@@ -1,13 +1,14 @@
 #include "DigiPot.h"
+#include "math.h"
 
 #define POT_0 0x00
 #define POT_1 0x10
 
 DigiPot::DigiPot(PinName miso, PinName mosi, PinName clk, PinName cs) {
     chsel = new DigitalOut(cs);
-    spi = new SPI(mosi, miso, clk);// mosi, miso, sclk
-    spi->format(8,0); // 8 bits , mode 0
-    spi->frequency(1000000); // Default SPI frequency. ( 250kHz maximum for read commands).
+    spi = new SPI(mosi, miso, clk);
+    spi->format(8,0); 
+    spi->frequency(1000000);
     level = 0x00;
 }
 
@@ -24,6 +25,7 @@ void DigiPot::setLevel(unsigned char level) {
     spi->write(POT_0);
     spi->write(level);
     *chsel = 1;
+    wait_us(2);
     *chsel = 0;
     spi->write(POT_1);
     spi->write(level);
